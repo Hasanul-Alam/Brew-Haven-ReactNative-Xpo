88d71fba19ec0f65c5a300d0bce65911833ee28e
Hasanul-Alam <superdev0000@gmail.com>
2024-10-17 23:25:49 +0600

    order history page design is done

--- app/home/(tabs)/orders.tsx
+++ app/home/(tabs)/orders.tsx
@@ -1,14 +1,100 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import { Octicons } from "@expo/vector-icons";
 
 const Orders = () => {
   return (
-    <View>
-      <Text>Orders</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-[#0C014]">
+      <ScrollView>
+        <View className="bg-[#0C0F14] pt-12 pb-5 min-h-screen w-full flex-1">
+          <View className="w-[85%] mx-auto">
+            {/* Order History Header */}
+            <View className="my-5">
+              <View className="flex-row justify-between items-center w-full mx-auto">
+                <View>
+                  <View className="bg-[#21262E] px-2 py-1 rounded-lg">
+                    <Octicons name="apps" color={"#ded9d9"} size={25} />
+                  </View>
+                </View>
+                <View>
+                  <Text className="text-2xl text-white font-semibold">
+                    Order History
+                  </Text>
+                </View>
+                <View className="w-[35px] h-[35px]">
+                  <Image
+                    className="w-full h-full rounded-xl"
+                    source={{
+                      uri: "https://i.ibb.co.com/jGMVDW2/coffee-shop.jpg",
+                    }}
+                  />
+                </View>
+              </View>
+            </View>
 
-export default Orders
+            {/* Order History Card */}
+            <View className="card-container mb-5 border border-1 border-[#D17842] py-3 px-2 rounded-xl">
+              {/* Date & Amount */}
+              <View className="flex-row justify-between items-center mb-3">
+                <View>
+                  <Text className="text-white text-base font-semibold">
+                    Order Date
+                  </Text>
+                  <Text className="text-white text-xs">20th March 2024</Text>
+                </View>
+                <View>
+                  <Text className="text-white text-base font-semibold">
+                    Total Amount
+                  </Text>
+                  <Text className="text-[#D17842] text-xs text-right">
+                    $ 74.40
+                  </Text>
+                </View>
+              </View>
 
-const styles = StyleSheet.create({})
+              {/* Main Card */}
+              <View className="bg-[#21262E] px-3 py-4 rounded-2xl">
+                <View className="flex-row justify-between items-center">
+                  <View className="flex-row gap-x-5 items-center">
+                    <View className="h-[57px] w-[57px] rounded-lg">
+                      <Image
+                        className="w-full h-full rounded-xl"
+                        source={{
+                          uri: "https://i.ibb.co.com/d085tCz/Details.png",
+                        }}
+                      />
+                    </View>
+                    <View>
+                      <Text className="text-base text-white font-semibold">
+                        Cappuccino
+                      </Text>
+                      <Text className="text-xs text-white">
+                        With Steamed Milk
+                      </Text>
+                    </View>
+                  </View>
+                  <View>
+                    <Text className="text-[#D17842] font-semibold text-xl">
+                      $ <Text className="text-white">37.20</Text>
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Orders;
+
+const styles = StyleSheet.create({});
